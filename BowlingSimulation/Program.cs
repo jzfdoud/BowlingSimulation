54769fdc3c86623a8aa821e756322fe0daea7e97
Jada <jada.fagan@gmail.com>
2020-06-24 15:27:21 -0400

    Add project files.

---
+++ BowlingSimulation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace BowlingSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            //var frame = new List<int>(10);
+            //for(var idx = 0; idx <10; idx++)
+            //{
+            var nbr = rnd.Next(0, 29);
+            //    frame.Add(nbr);
+            //}
+
+            var frame = new List<int>();
+            {
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+                frame.Add(nbr);
+            }
+
+
+            var Total = 0;
+
+            foreach (var item in frame)
+                {
+                Total += item;
+                }
+            Console.WriteLine($"Score is {Total}.");
+
+
+        }
+    }
+}
